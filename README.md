c8c475c1404414fe075224a55a8652df0ef9b0ce
NEDONION <nedjiachenghu@gmail.com>
2022-01-26 17:27:56 -0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# jira-mooc-Chinese
+# 2021 必修：React + React Hook + TS 最佳实践仿 Jira 企业级项目
+
+Hi，同学们好，感谢大家的信任购买这个课程。查看课程对应代码请 checkout 到对应的 commit
+
+这里是配套课件的链接：[点击我查看配套课件](https://www.notion.so/React-491ad0643476437cafde50bee4dde6ed)
---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { useAuth } from "context/auth-context";
+import { ErrorBoundary } from "components/error-boundary";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
+
+const AuthenticatedApp = React.lazy(() => import("authenticated-app"));
+const UnauthenticatedApp = React.lazy(() => import("unauthenticated-app"));
+
+function App() {
+  const { user } = useAuth();
+
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        <React.Suspense fallback={<FullPageLoading />}>
+          {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+        </React.Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+}
+
+export default App;
